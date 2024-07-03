8849df9043716589826204a1dee43597310e8cd1
abhishekkatnoria <katnoriaabhishek@rwit.io>
2024-07-03 18:31:26 +0530

    Set the all component to the Home page

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,36 +1,54 @@
-import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import Head from "next/head";
-import { isFilled } from "@prismicio/client";
-import { SliceZone } from "@prismicio/react";
+import Image from "next/image";
+import { Inter, Raleway, Plus_Jakarta_Sans } from "next/font/google";
+import styles from "@/styles/Home.module.scss";
+import TopNavBar from "@/components/TopNavbar";
+import Navbar from "@/components/Navbar";
 
-import { components } from "@/slices/index";
-import { createClient } from "@/prismicio";
+import {
+  Faqs,
+  bannerData,
+  contact,
+  footer,
+  history,
+  navbar,
+  ourBrands,
+  ourClient,
+  ourProjects,
+  score,
+  topNav,
+} from "@/mock.data";
+import Score from "@/components/Score";
+import History from "@/components/History";
+import OurBrands from "@/components/OurBrands";
+import OurClients from "@/components/OurClients";
+import OurProjects from "@/components/OurProjects";
+import FAQ from "@/components/FAQ";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+import Banner from "@/components/Banner";
 
-export default function Page({
-  page,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+const jakarta = Plus_Jakarta_Sans({
+  variable: "--font-jakarta",
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
+
+const Home = () => {
   return (
-    <>
-      <Head>
-        <title>{page.data.meta_title}</title>
-        {isFilled.keyText(page.data.meta_description) ? (
-          <meta name="description" content={page.data.meta_description} />
-        ) : null}
-      </Head>
-      <SliceZone slices={page.data.slices} components={components} />
-    </>
+    <main className={`${jakarta.variable}`}>
+      <TopNavBar props={topNav} />
+      <Navbar props={navbar} />
+      <Banner props={bannerData}/>
+      <Score props={score} />
+      <History props={history} />
+      <OurBrands props={ourBrands} />
+      <OurClients props={ourClient} />
+      <OurProjects props={ourProjects} />
+      <FAQ props={Faqs} />
+      <Contact props={contact} /> 
+      <Footer props={footer}/>
+    </main>
   );
-}
-
-export async function getStaticProps({ previewData }: GetStaticPropsContext) {
-  // The `previewData` parameter allows your app to preview
-  // drafts from the Page Builder.
-  const client = createClient({ previewData });
-
-  // The query fetches the page's data based on the current URL.
-  const page = await client.getSingle("Home");
-
-  return {
-    props: { page },
-  };
-}
+};
+export default Home;
