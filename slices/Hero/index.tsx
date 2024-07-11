426ff754c67745f338e79a627acf40689a212135
abhishekkatnoria <katnoriaabhishek@rwit.io>
2024-07-11 13:40:48 +0530

    Optional channing and add a class for the Banner image

--- slices/Hero/index.tsx
+++ slices/Hero/index.tsx
@@ -1,4 +1,4 @@
-import { Content, asImageSrc } from "@prismicio/client";
+import { Content, asImageSrc, asLink } from "@prismicio/client";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import styles from "../../components/Banner/banner.module.scss";
@@ -17,7 +17,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
   return (
     <div className={`${styles.banner} container-full`}>
       <div
-        className="banner__cont"
+        className={`banner__cont ${asImageSrc(primary?.BackgroundImage) ? "banner__hero-image" : ""}`}
         style={{
           backgroundImage: asImageSrc(primary?.BackgroundImage)
             ? `linear-gradient(
@@ -30,32 +30,34 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
             : "",
         }}
       >
-        <div className="banner__wrap">
-          {primary?.Heading && (
-            <h2 className="banner__title">{primary?.Heading}</h2>
-          )}
-          {asText(primary?.Description) && (
-            <PrismicRichText
-              field={primary?.Description}
-              components={{
-                paragraph: ({ children }) => (
-                  <p className="banner__desc">{children}</p>
-                ),
-              }}
-            />
-          )}
-          {primary?.Button.map((items, index) => {
-            return (
-              <PrismicNextLink
-                field={items?.ButtonLink}
-                className="banner__btn"
-                key={index}
-              >
-                {items?.Label}
-              </PrismicNextLink>
-            );
-          })}
-        </div>
+        {primary?.Heading || asText(primary?.Description) || primary?.Button ? (
+          <div className="banner__wrap">
+            {primary?.Heading && (
+              <h2 className="banner__title">{primary?.Heading}</h2>
+            )}
+            {asText(primary?.Description) && (
+              <PrismicRichText
+                field={primary?.Description}
+                components={{
+                  paragraph: ({ children }) => (
+                    <p className="banner__desc">{children}</p>
+                  ),
+                }}
+              />
+            )}
+            {primary?.Button.map((items, index) => {
+              return (
+                <PrismicNextLink
+                  field={items?.ButtonLink}
+                  className="banner__btn"
+                  key={index}
+                >
+                  {items?.Label}
+                </PrismicNextLink>
+              );
+            })}
+          </div>
+        ) : null}
       </div>
     </div>
   );
