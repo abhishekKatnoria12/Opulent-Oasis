428c33dffcfe712a0edd52f4bbf5f6e8bdfa0bc8
abhishekkatnoria <katnoriaabhishek@rwit.io>
2024-07-11 19:45:57 +0530

    Optional Chaining of FAQs section

--- slices/Faqs/index.tsx
+++ slices/Faqs/index.tsx
@@ -3,6 +3,7 @@ import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import styles from "../../components/FAQ/faq.module.scss";
 import { useState } from "react";
 import { PrismicNextImage } from "@prismicio/next";
+import { asText } from "@prismicio/client/richtext";
 
 /**
  * Props for `Faqs`.
@@ -22,45 +23,55 @@ const Faqs = ({ slice }: FaqsProps): JSX.Element => {
 
   return (
     <div className={`${styles.faq} container-sm`}>
-      <div className="faq__cont">
-        <div className="faq__wrap">
-          <div className="faq__heading">
-            <PrismicRichText field={primary?.Heading} />
-          </div>
-          <p className="faq__desc">{primary?.SubHeading}</p>
-        </div>
-
-        <div className="faq__acc-cont">
-          {primary?.Content.map((items, index) => (
-            <div
-              className={`${
-                index !== activeIndex
-                  ? "faq__acc-wrap"
-                  : "faq__acc-wrap faq__acc-wrap-var"
-              }`}
-              key={index}
-              onClick={() => handleClick(index)}
-            >
-              <div className="faq__arrow">
-                <div className="faq__title">
-                  <PrismicRichText field={items?.Title} />
-                </div>
-                <PrismicNextImage
-                  field={
-                    index === activeIndex ? primary?.SubIcon : primary?.AddIcon
-                  }
-                  height={2}
-                  width={12}
-                  className="faq__logo"
-                />
-              </div>
-              {index === activeIndex && (
-                <p className="faq__subTitle">{items?.SubTitle}</p>
-              )}
+      {asText(primary?.Heading) ||
+      primary?.SubHeading ||
+      primary?.Content.length > 0 ? (
+        <div className="faq__cont">
+          <div className="faq__wrap">
+            <div className="faq__heading">
+              <PrismicRichText field={primary?.Heading} />
             </div>
-          ))}
+            <p className="faq__desc">{primary?.SubHeading}</p>
+          </div>
+
+          {primary?.Content.length > 0 && (
+            <div className="faq__acc-cont">
+              {primary?.Content.map((items, index) => (
+                <div
+                  className={`${
+                    index !== activeIndex
+                      ? "faq__acc-wrap"
+                      : "faq__acc-wrap faq__acc-wrap-var"
+                  }`}
+                  key={index}
+                  onClick={() => handleClick(index)}
+                >
+                  <div className="faq__arrow">
+                    {asText(items?.Title) && (
+                      <div className="faq__title">
+                        <PrismicRichText field={items?.Title} />
+                      </div>
+                    )}
+                    <PrismicNextImage
+                      field={
+                        index === activeIndex
+                          ? primary?.SubIcon
+                          : primary?.AddIcon
+                      }
+                      height={2}
+                      width={12}
+                      className="faq__logo"
+                    />
+                  </div>
+                  {index === activeIndex && (
+                    <p className="faq__subTitle">{items?.SubTitle}</p>
+                  )}
+                </div>
+              ))}
+            </div>
+          )}
         </div>
-      </div>
+      ) : null}
     </div>
   );
 };
