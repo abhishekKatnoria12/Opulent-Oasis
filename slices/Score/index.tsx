381c573b6728bd861410ca235bf166536af9ae5b
abhishekkatnoria <katnoriaabhishek@rwit.io>
2024-07-12 10:42:12 +0530

    Optional Channing of Score Section

--- slices/Score/index.tsx
+++ slices/Score/index.tsx
@@ -23,15 +23,19 @@ const Score = ({ slice }: ScoreProps): JSX.Element => {
                 (item) => item?.Heading || item?.SubHeading
               ).map((items, index) => (
                 <div className="score__items" key={index}>
-                  <h3 className="score__heading">{items?.Heading}</h3>
-                  <PrismicRichText
-                    field={items?.SubHeading}
-                    components={{
-                      paragraph: ({ children }) => (
-                        <p className="score__subheading">{children}</p>
-                      ),
-                    }}
-                  />
+                  {items?.Heading && (
+                    <h3 className="score__heading">{items?.Heading}</h3>
+                  )}
+                  {asText(items?.SubHeading) && (
+                    <PrismicRichText
+                      field={items?.SubHeading}
+                      components={{
+                        paragraph: ({ children }) => (
+                          <p className="score__subheading">{children}</p>
+                        ),
+                      }}
+                    />
+                  )}
                 </div>
               ))}
             </div>
