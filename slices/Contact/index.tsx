948c4d4e43e2dd115d29dd51ac2379e59d810b7e
abhishekkatnoria <katnoriaabhishek@rwit.io>
2024-07-12 10:38:10 +0530

    Optional Chaining of Contact form

--- slices/Contact/index.tsx
+++ slices/Contact/index.tsx
@@ -3,6 +3,8 @@ import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import styles from "../../components/Contact/contact.module.scss";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import Link from "next/link";
+import { asImageSrc } from "@prismicio/client";
+import { asText } from "@prismicio/client/richtext";
 
 /**
  * Props for `Contact`.
@@ -17,59 +19,71 @@ const Contact = ({ slice }: ContactProps): JSX.Element => {
   const { primary } = slice;
   return (
     <div className={`${styles.contact} container-sm`}>
-      <div className="contact__cont">
-        <div className="contact__wrap">
-          {primary?.ContactImage && (
-            <div className="contact__banner">
-              <PrismicNextImage
-                field={primary?.ContactImage}
-                className="contact__image"
-              />
-            </div>
-          )}
+      {asImageSrc(primary?.ContactImage) ||
+      asText(primary?.Heading) ||
+      primary?.SubHeading ||
+      primary?.ContactForm.length > 0 ||
+      primary?.Button.length > 0 ? (
+        <div className="contact__cont">
+          <div className="contact__wrap">
+            {primary?.ContactImage && (
+              <div className="contact__banner">
+                <PrismicNextImage
+                  field={primary?.ContactImage}
+                  className="contact__image"
+                />
+              </div>
+            )}
 
-          <div className="contact__form-cont">
-            <div className="contact__title">
-              <PrismicRichText field={primary?.Heading} />
-            </div>
-
-            <p className="contact__desc">{primary?.SubHeading}</p>
-
-            <form
-              method="POST"
-              action="https://getform.io/f/byvppkza"
-              className="contact__form-wrap"
-            >
-              {primary?.ContactForm.map((items, index) => (
-                <div key={index} className="contact__form-tag">
-                  <label className="contact__label">{items?.Label}</label>
-                  {items.Type === "textarea" ? (
-                    <textarea
-                      className="contact__input-field"
-                      placeholder={items.Placeholder?.toString()}
-                      name={items.Placeholder?.toString()}
-                      rows={2}
-                      cols={40}
-                    />
-                  ) : (
-                    <input
-                      className="contact__input-field"
-                      type={items.Type?.toString()}
-                      name={items.Type?.toString()}
-                      placeholder={items.Placeholder?.toString()}
-                    />
-                  )}
+            <div className="contact__form-cont">
+              {asText(primary?.Heading) && (
+                <div className="contact__title">
+                  <PrismicRichText field={primary?.Heading} />
                 </div>
-              ))}
-              {primary?.Button.map((items, indexs) => (
-                <button className="contact__btn" key={indexs}>
-                  {items?.Text}
-                </button>
-              ))}
-            </form>
+              )}
+
+              {primary?.SubHeading && (
+                <p className="contact__desc">{primary?.SubHeading}</p>
+              )}
+
+              {primary?.ContactForm.length > 0 && (
+                <form
+                  method="POST"
+                  action="https://getform.io/f/byvppkza"
+                  className="contact__form-wrap"
+                >
+                  {primary?.ContactForm.map((items, index) => (
+                    <div key={index} className="contact__form-tag">
+                      <label className="contact__label">{items?.Label}</label>
+                      {items.Type === "textarea" ? (
+                        <textarea
+                          className="contact__input-field"
+                          placeholder={items.Placeholder?.toString()}
+                          name={items.Placeholder?.toString()}
+                          rows={2}
+                          cols={40}
+                        />
+                      ) : (
+                        <input
+                          className="contact__input-field"
+                          type={items.Type?.toString()}
+                          name={items.Type?.toString()}
+                          placeholder={items.Placeholder?.toString()}
+                        />
+                      )}
+                    </div>
+                  ))}
+                  {primary?.Button.map((items, indexs) => (
+                    <button className="contact__btn" key={indexs}>
+                      {items?.Text}
+                    </button>
+                  ))}
+                </form>
+              )}
+            </div>
           </div>
         </div>
-      </div>
+      ) : null}
     </div>
   );
 };
